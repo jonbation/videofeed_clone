54f09699675ef061b9b393aa44ddc3ad3f46d43e
jonbation <jonbation4@gmail.com>
2025-02-20 12:54:58 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_video_feed/core/di/dependency_injector.dart';
 import 'package:flutter_video_feed/core/init/app_widget.dart';
+import 'package:flutter_video_feed/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
